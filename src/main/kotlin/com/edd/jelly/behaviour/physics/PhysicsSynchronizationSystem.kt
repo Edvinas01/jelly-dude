e456a368cf8ccdfa57823a3f78dc947d4b27c0db
Edvinas <edvinas108@gmail.com>
2017-04-23 20:39:51 +0300

    Playing with angles

--- src/main/kotlin/com/edd/jelly/behaviour/physics/PhysicsSynchronizationSystem.kt
+++ src/main/kotlin/com/edd/jelly/behaviour/physics/PhysicsSynchronizationSystem.kt
@@ -5,6 +5,7 @@ import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.utils.ImmutableArray
+import com.badlogic.gdx.math.MathUtils
 import com.edd.jelly.behaviour.components.Transform
 import com.edd.jelly.behaviour.components.transform
 import com.edd.jelly.behaviour.physics.body.SoftBody
@@ -12,6 +13,7 @@ import com.edd.jelly.behaviour.rendering.SoftRenderable
 import com.edd.jelly.util.degrees
 import com.edd.jelly.util.toVec2
 import com.google.inject.Inject
+import org.jbox2d.common.Vec2
 
 
 class PhysicsSynchronizationSystem @Inject constructor() : EntitySystem() {
@@ -74,11 +76,22 @@ class PhysicsSynchronizationSystem @Inject constructor() : EntitySystem() {
         softRenderables.forEach {
             val transform = it.transform
             val vertices = SoftRenderable[it].region.vertices
-            val bodies = SoftBody[it].bodies
+            val softBody = SoftBody[it]
+            val bodies = softBody.bodies
 
             val center = transform.position.toVec2()
 
+//            atan2(P2.y - P1.y, P2.x - P1.x) - atan2(P3.y - P1.y, P3.x - P1.x)
+
+            val p1 = softBody.pivots[2].getLocalPoint(center)
+            val p2 = softBody.pivots[0].getLocalPoint(center)
+            val p3 = softBody.pivots[1].getLocalPoint(center)
+
+            val angle = (MathUtils.atan2(p2.y - p1.y, p2.x - p1.x) - MathUtils.atan2(p3.y - p1.y, p3.x - p1.x)) * MathUtils.radiansToDegrees
+            println(angle)
+
             bodies.forEachIndexed { i, b ->
+
                 val pos = b.getLocalPoint(center).negateLocal()
                 vertices[i * 2] = pos.x
                 vertices[i * 2 + 1] = pos.y
--- src/main/kotlin/com/edd/jelly/behaviour/physics/body/SoftBody.kt
+++ src/main/kotlin/com/edd/jelly/behaviour/physics/body/SoftBody.kt
@@ -5,7 +5,8 @@ import com.edd.jelly.behaviour.components.ComponentResolver
 import org.jbox2d.dynamics.Body
 
 data class SoftBody(
-        val bodies: List<Body>
+        val bodies: List<Body>,
+        val pivots: List<Body> = emptyList()
 ) : Component {
 
     companion object : ComponentResolver<SoftBody>(SoftBody::class.java)
