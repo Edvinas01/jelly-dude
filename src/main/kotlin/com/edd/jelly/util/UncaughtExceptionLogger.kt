c196508a4134c647cda4a175d25a1ed01342fc52
Edvinas <edvinas108@gmail.com>
2017-05-14 12:43:59 +0300

    Add error report dialog

--- src/main/kotlin/com/edd/jelly/util/UncaughtExceptionLogger.kt
+++ src/main/kotlin/com/edd/jelly/util/UncaughtExceptionLogger.kt
@@ -1,7 +1,9 @@
 package com.edd.jelly.util
 
 import com.edd.jelly.behaviour.physics.PhysicsDebugRenderer
+import com.edd.jelly.core.configuration.Configurations
 import org.apache.logging.log4j.LogManager
+import javax.swing.SwingUtilities
 
 class UncaughtExceptionLogger : Thread.UncaughtExceptionHandler {
 
@@ -10,6 +12,13 @@ class UncaughtExceptionLogger : Thread.UncaughtExceptionHandler {
     }
 
     override fun uncaughtException(t: Thread, e: Throwable) {
+
         LOG.fatal("Uncaught exception for thread: {}", t, e)
+        SwingUtilities.invokeLater {
+            ErrorReport(Configurations.GIT_HUB_NEW_ISSUE_URL, e).apply {
+                pack()
+                isVisible = true
+            }
+        }
     }
 }
