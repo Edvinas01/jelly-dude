3d93f883810f862f48c76c3eef622de2d5369faf
Edvinas <edvinas108@gmail.com>
2017-05-18 21:10:47 +0300

    Use code solution for forms, fix map a bit

--- src/main/kotlin/com/edd/jelly/util/UncaughtGameExceptionHandler.kt
+++ src/main/kotlin/com/edd/jelly/util/UncaughtGameExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.edd.jelly.util
 import com.edd.jelly.behaviour.physics.PhysicsDebugRenderer
 import com.edd.jelly.core.configuration.Configurations
 import org.apache.logging.log4j.LogManager
+import javax.swing.SwingUtilities
 
 class UncaughtGameExceptionHandler : Thread.UncaughtExceptionHandler {
 
@@ -13,10 +14,12 @@ class UncaughtGameExceptionHandler : Thread.UncaughtExceptionHandler {
     override fun uncaughtException(t: Thread, e: Throwable) {
 
         LOG.fatal("Uncaught exception for thread: {}", t, e)
-        // showErrorDialog(e) only works from IDEA
+        showErrorDialog(e)
     }
 
     private fun showErrorDialog(e: Throwable) {
-        ErrorReport(Configurations.GIT_HUB_NEW_ISSUE_URL, e).isVisible = true
+        SwingUtilities.invokeLater {
+            CrashReport.showReport(Configurations.GIT_HUB_NEW_ISSUE_URL, e)
+        }
     }
 }
