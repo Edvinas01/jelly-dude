02e60fe56d9fc26b1a4cde4711689098d21d230a
Edvinas <edvinas108@gmail.com>
2017-05-23 09:41:33 +0300

    fixz

--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -4,6 +4,10 @@
 var Player = new function () {
     var deflation = Java.type('com.edd.jelly.behaviour.player.Player.Deflation');
 
+    var totalTime = 0;
+    var interval = 1;
+    var grow = true;
+
     /**
      * Player deflation states.
      */
@@ -33,5 +37,23 @@ var Player = new function () {
      */
     this.fly = function (player) {
         player.airTime = 0;
+    };
+
+    /**
+     * Constantly shrinks and grows the player.
+     */
+    this.shrinkAndGrow = function (player) {
+        if (totalTime > interval) {
+            totalTime = 0;
+            grow = !grow;
+        }
+
+        if (grow) {
+            Player.grow(player);
+        } else {
+            Player.shrink(player);
+        }
+
+        totalTime += game.deltaTime;
     };
 };
--- assets/scripts/movement.js
+++ assets/scripts/movement.js
@@ -1,33 +1,8 @@
 load('assets/scripts/Player.js');
 
 function main() {
-
-    var totalTime = 0;
-    var interval = 1;
-    var grow = true;
-
-    /**
-     * Grows and shrink the player constantly.
-     */
-    function shrinkAndGrow(player) {
-        if (totalTime > interval) {
-            totalTime = 0;
-            grow = !grow;
-        }
-
-        if (grow) {
-            Player.grow(player);
-        } else {
-            Player.shrink(player);
-        }
-
-        totalTime += game.deltaTime;
-    }
-
     return {
         beforeMove: function (player) {
-            // shrinkAndGrow(player);
-            // Player.fly(player);
         }
     }
 }
